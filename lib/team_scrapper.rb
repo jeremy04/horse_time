22064d7c00261ba0d3eb50417d87c9fe0eab387c
Jeremy Lipson <jeremy04@gmail.com>
2015-12-21 20:46:57 -0500

    Time.now is not a good idea on heroku

--- lib/team_scrapper.rb
+++ lib/team_scrapper.rb
@@ -8,10 +8,8 @@ class AvailableGames
   def self.games
     agent = Mechanize.new
     page = agent.get("http://www.nicetimeonice.com/api/seasons/20152016/games/")
-    logger = Logger.new($stdout)
-    logger.info(page.content)
     json = JSON.parse(page.content)
-    games = json.select { |h| Date.parse(h["date"]) == Time.now.to_date }.map {|x| [x["homeTeam"],x["awayTeam"]] }.sort
+    games = json.select { |h| Date.parse(h["date"]) == (Time.now.utc + Time.zone_offset("EST")).to_date }.map {|x| [x["homeTeam"],x["awayTeam"]] }.sort
     
     games
   end

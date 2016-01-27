e77a7c57e534e718a50f3ccb20d40dfa78446c01
Jeremy Lipson <jeremy04@gmail.com>
2016-01-27 00:17:55 -0500

    wow eastern time is not soo good for late games

--- lib/team_scrapper.rb
+++ lib/team_scrapper.rb
@@ -9,7 +9,7 @@ class AvailableGames
     agent = Mechanize.new
     page = agent.get("http://www.nicetimeonice.com/api/seasons/20152016/games/")
     json = JSON.parse(page.content)
-    games = json.select { |h| Date.parse(h["date"]) == (Time.now.utc + Time.zone_offset("EST")).to_date }.map {|x| [x["homeTeam"],x["awayTeam"]] }.sort
+    games = json.select { |h| Date.parse(h["date"]) == (Time.now.utc + Time.zone_offset("PST")).to_date }.map {|x| [x["homeTeam"],x["awayTeam"]] }.sort
     
     games
   end

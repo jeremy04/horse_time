e9c68949760abd322be061abf21118a8694a46ac
Jeremy Lipson <jeremy04@gmail.com>
2016-10-08 15:26:51 -0400

    added check for current team

--- lib/active_roster.rb
+++ lib/active_roster.rb
@@ -26,8 +26,8 @@ class ActiveRoster
     jsonData = JSON.parse(page.body)
     players = jsonData["gameData"]["players"].map { |p| p[1] }
     if players.size > 0
-      home_skaters = players.select { |p| p["currentTeam"]["name"] == latest_game["homeTeam"] }.map { |p| p["fullName"] }
-      away_skaters = players.select { |p| p["currentTeam"]["name"] == latest_game["awayTeam"] }.map { |p| p["fullName"] }
+      home_skaters = players.select { |p| p["currentTeam"] && p["currentTeam"]["name"] == latest_game["homeTeam"] }.map { |p| p["fullName"] }
+      away_skaters = players.select { |p| p["currentTeam"] && p["currentTeam"]["name"] == latest_game["awayTeam"] }.map { |p| p["fullName"] }
     else
       home_team_id = jsonData["gameData"]["teams"]["home"]["id"]
       away_team_id = jsonData["gameData"]["teams"]["away"]["id"]

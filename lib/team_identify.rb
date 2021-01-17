f1622ecaca4ac0ba28419557dc5becafc8dcc090
Jeremy Lipson <jeremy.lipson@arcadiasolutions.com>
2021-01-17 13:18:18 -0500

    refactor

--- lib/team_identify.rb
+++ lib/team_identify.rb
@@ -1,9 +1,9 @@
-require 'net/https'
-require 'json'
-require 'nokogiri'
-require 'active_support/all'
-require './lib/cache_wrapper'
-require './lib/available_games'
+require "net/https"
+require "json"
+require "nokogiri"
+require "active_support/all"
+require "./lib/cache_wrapper"
+require "./lib/available_games"
 
 class TeamIdentify
   attr_reader :other_team
@@ -19,39 +19,30 @@ class TeamIdentify
     json = @json
 
     # Get latest game
-    latest_game = horse_games(json).select { |h| Date.parse(h["date"]) == (@date.utc + Time.zone_offset("-10")).to_date }.first
+    latest_game = horse_games(json).find {|h| Date.parse(h["date"]) == (@date.utc + Time.zone_offset("-10")).to_date }
     team = determine_other_team(latest_game)
     @other_team = team
     [get_link(@horse_team), get_link(team)]
   end
 
-  private 
+  private
 
   def horse_games(json)
-    json.select { |x| x["awayTeam"] == @horse_team || x["homeTeam"] == @horse_team }
+    json.select {|x| x["awayTeam"] == @horse_team || x["homeTeam"] == @horse_team }
   end
 
   def get_link(team)
-    uri = URI("https://www.dailyfaceoff.com/teams/")
-    http = Net::HTTP.new(uri.host, uri.port)
-    http.use_ssl = true
-    http.ssl_version = :TLSv1_2
-    http.verify_mode = OpenSSL::SSL::VERIFY_PEER
-    page = http.get(uri.request_uri)
-    doc = Nokogiri::HTML(page.body)
-    # Fix montreal canadiens on Daily faceoff
-    team = team.gsub('é','e')
-    link = doc.css(".site-main_primary.columns a").map { |x| x.attributes["href"].value }.select { |x| x =~ /#{team.gsub(/\W/," ").squish.gsub(" ", "-").downcase}/  }
-    "#{link.first}"
+    team = team.tr("é", "e")
+    team = team.gsub(/\W/, " ").squish.tr(" ", "-").downcase
+    "https://www.dailyfaceoff.com/teams/#{team}/line-combinations"
   end
 
   def determine_other_team(element)
     team = if element["awayTeam"] != @horse_team
-              element["awayTeam"]
+             element["awayTeam"]
            else
-              element["homeTeam"]
+             element["homeTeam"]
           end
     team
   end
-
 end

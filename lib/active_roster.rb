6297e3349f2b8ae0a6dc9e4a9a47f1d2b6884b82
Jeremy Lipson <jeremy.lipson@arcadiasolutions.com>
2022-10-08 11:21:17 -0400

    chinese bot stuff

--- lib/active_roster.rb
+++ lib/active_roster.rb
@@ -81,24 +81,24 @@ class ActiveRoster
       Puppeteer.launch **options do |browser|
         page = browser.new_page
         page.viewport = Puppeteer::Viewport.new(width: 1280, height: 800)
-        # page.evaluate_on_new_document(<<~JAVASCRIPT)
-        # () => {
-        #   Object.defineProperty(navigator, "webdriver", {get: () => false});
-        #   Object.defineProperty(window, "webdriver", {get: () => false});
-        #   window.navigator.chrome = {
-        #       runtime: {},
-        #   };
-        #   Object.defineProperty(navigator, 'platform', {
-        #       get: () => "Win32",
-        #   });
+        page.evaluate_on_new_document(<<~JAVASCRIPT)
+        () => {
+          Object.defineProperty(navigator, "webdriver", {get: () => false});
+          Object.defineProperty(window, "webdriver", {get: () => false});
+          window.navigator.chrome = {
+              runtime: {},
+          };
+          Object.defineProperty(navigator, 'platform', {
+              get: () => "Win32",
+          });
 
-        #   Object.defineProperty(navigator, 'plugins', {
-        #       get: () => [1, 2],
-        #   });
+          Object.defineProperty(navigator, 'plugins', {
+              get: () => [1, 2],
+          });
 
-        # }
-        # JAVASCRIPT
-        #page.user_agent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.39 Safari/537.36"
+        }
+        JAVASCRIPT
+        page.user_agent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.39 Safari/537.36"
         page.goto("https://dailyfaceoff.com/teams/#{team.downcase.gsub(/\s/,"-")}/line-combinations/", wait_until: 'domcontentloaded')
         doc = Nokogiri::HTML(page.content)
         forwards = doc.css("#forwards").css(".player-name").map { |name| name.text }
